2d1fa4c486b3e92e20bab10f3e915e7010925d98
EsbernTK <esberntk@gmail.com>
2026-04-10 22:27:58 +0200

    added shader!

--- nothing/Scripts/Main.gd
+++ nothing/Scripts/Main.gd
@@ -1,8 +1,10 @@
-extends Node
+extends Node2D
 
 var score = 0
 
 @export var particle_scene: PackedScene;
+
+@onready var field: Sprite2D = get_node("Field")
 
 var is_pressed: bool = false
 
@@ -38,8 +40,17 @@ func _spawn_particle_pair() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		var mouse_event = event as InputEventMouseButton
+		var position = get_viewport().get_mouse_position()
+		var position_uv = position / get_viewport_rect().size
+		
+		if mouse_event.is_pressed():
+			print(position_uv)
+			field.material.set_shader_parameter("target", position_uv)
+		
 		if mouse_event.is_pressed() and !is_pressed:
 			is_pressed = true
+			
 			_spawn_particle_pair()
 		if event.is_released():
 			is_pressed = false
+			field.material.set_shader_parameter("target", Vector2(-1,-1))
